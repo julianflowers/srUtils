9b7284f4ff53caa7f36ce24e843ab85f125f94de
Julian Flowers <julian.flowers12@gmail.com>
2022-10-04 08:00:16 +0100

    updates

--- R/extract_locations.R
+++ R/extract_locations.R
@@ -7,16 +7,39 @@
 #'
 #' @examples extract_location(text)
 
-extract_location <- function(text){
+extract_location <- function(text, doc_id){
+
+  options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8192m"))
+  gc()  ## https://stackoverflow.com/questions/34624002/r-error-java-lang-outofmemoryerror-java-heap-space
 
   if (!require("pacman")) install.packages("pacman")
   pacman::p_load_gh("trinker/entity")
   require(dplyr)
   require(tidyr)
+  require(purrr)
+  require(tictoc)
 
-  text |>
-    mutate(loc = map(value, location_entity)) |>
-    unnest("loc") |>
-    unnest_auto("loc")
+  tic()
+
+  safe_loc <- purrr::safely(location_entity)
+
+  locs <- safe_loc(text) %>%
+    .["result"] |>
+    enframe() |>
+    mutate(file = doc_id)
+
+  toc()
+
+  locs
 
 }
+
+# rte$text[1]
+# t <- map2_df(rte$text, rte$doc_id, extract_location)
+# t |>
+#   filter(value != "NULL") |>
+#   unnest("value") |>
+#   filter(value != "NULL") |>
+#   unnest("value")
+#
+# location_entity(rte$text[2])
